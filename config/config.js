c9b356b08c0c79db5cf3e77fafec851b055732af
Romna <r.maiorkhak@gmail.com>
2024-07-18 10:00:21 +0000

    initial commit

--- config/config.js
+++ config/config.js
@@ -1,6 +1,10 @@
 /* eslint-disable no-undef */
 require('dotenv').config();
-
+console.log("username:" +  process.env.DB_USERNAME)
+console.log("password:" +  process.env.DB_PASSWORD)
+console.log("database:" +  process.env.DB_NAME)
+console.log("host:" +  process.env.DB_HOST)
+console.log("use_env_variable:" +  process.env.DEV_DATABASE_URL)
 module.exports = {
     development: {
         username: process.env.DB_USERNAME,

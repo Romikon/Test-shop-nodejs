aebedf73934c91ae6a6386aa94df3cf1147bf3cc
Romna <r.maiorkhak@gmail.com>
2024-07-18 12:35:09 +0000

    initial commit

--- config/config.js
+++ config/config.js
@@ -3,7 +3,9 @@ require('dotenv').config();
 
 module.exports = {
     development: {
-        
+        username: process.env.DB_USERNAME || "admin",
+        password: process.env.DB_PASSWORD || 123123,
+        database: process.env.DB_NAME || "shop",
         host: process.env.DB_HOST || "localhost",
         dialect: "postgres"
     },

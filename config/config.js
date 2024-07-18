9ba948e63750921b5e549be2ce2386f57fcefff7
Romna <r.maiorkhak@gmail.com>
2024-07-18 10:33:55 +0000

    initial commit

--- config/config.js
+++ config/config.js
@@ -7,12 +7,12 @@ console.log("host:" +  process.env.DB_HOST)
 console.log("use_env_variable:" +  process.env.DEV_DATABASE_URL)
 module.exports = {
     development: {
-        username: process.env.DB_USERNAME,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
-        host: process.env.DB_HOST,
+        username: process.env.DB_USERNAME || "admin",
+        password: process.env.DB_PASSWORD || 123123,
+        database: process.env.DB_NAME || "shop",
+        host: process.env.DB_HOST || "localhost",
         dialect: "postgres",
-        use_env_variable: process.env.DEV_DATABASE_URL
+        use_env_variable: process.env.DEV_DATABASE_URL || "postgres://admin:123123@127.0.0.1:5433/shop"
     },
     test: {
         username: process.env.DB_USERNAME,
